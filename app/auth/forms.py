de44dc9cc6c614dc34652dc5d921c12fee9ba9da
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-28 22:33:19 +0300

    authenticate a registration class form by providing entries that the user is required of so as to proceed

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,Email,ValidationError
+
+from app.models import User
+
+class RegisterForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(),Length(min=2, max=20)])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    confirm_password = PasswordField('confirm_password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
