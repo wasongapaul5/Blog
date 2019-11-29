653d6b4bbb01ed8da387b7d9fe0572bffc364a6c
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-29 10:03:19 +0300

    set the debug mode to true

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
-from app import creat_app
-from app.models import User
 from flask_migrate import Migrate,MigrateCommand
 from flask_script import Manager,Server
+from app.models import User
+
+from app import create_app
 
 app = create_app()
 
@@ -10,3 +11,6 @@ manager.add_command('server', Server(use_debugger=True))
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
