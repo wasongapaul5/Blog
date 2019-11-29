a7d761c6e8891596aacacd8f70fcad2920771156
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-29 16:25:53 +0300

    correct on the String placement

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ from app import login_manager
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.Sring(255), nullable=True, unique=True)
+    username = db.Column(db.String(255), nullable=True, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     image_file = db.Column(db.String(255), nullable=False, default='default.jpg')
