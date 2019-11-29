f1969171fe251130740904c63ee97b37cdfa0af8
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-29 10:01:23 +0300

    register blueprint instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,25 @@
 from flask import Flask
-
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
+from config import config
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+bootstrap = Bootstrap(app)
+mail = Mail(app)
+login_manager.login_view = 'auth.login'
+login_manager.session_protection = 'strong'
+login_manager.login_message = 'info'
+
+
+def create_app():
+    app.config.from_object(Config)
+    from .auth import auth as auth_blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(main_blueprint)
+    return app
