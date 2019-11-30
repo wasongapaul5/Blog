9f5f1eaa9e23858ee8474bb10159a8b165aa0ff0
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-30 10:04:10 +0300

    add a main route function that returns the index page its data

--- app/__init__.py
+++ app/__init__.py
@@ -4,23 +4,33 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_wtf.csrf import CSRFProtect
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
 login_manager = LoginManager()
-login_manager.init_app(app)
-bootstrap = Bootstrap(app)
-mail = Mail(app)
 login_manager.login_view = 'auth.login'
 login_manager.session_protection = 'strong'
-login_manager.login_message = 'info'
 
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+csrf = CSRFProtect()
 
-def create_app(config_name):
+def create_app():
+    app = Flask(__name__)
+
     app.config.from_object(Config)
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix='/auth')
-    from .main import main as main_blueprint
+    app.config['SECRET_KEY']= 'VGACDGWAGHV'
 
+    db.init_app =(app)
+    login_manager.init_app(app)
+    bootstrap.init_app = Bootstrap(app)
+    mail.init_app(app)
+    csrf.init_app(app)
+    
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
     return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,32 @@
-from flask import render_template
+from flask import render_template,url_for,redirect,request,flash,abort
+import os
+import secrets
+from app import app,db
+from app.main.forms import UpdateAccountForm,PostForm
+from app.models import Post,Clap,Comment
+from app.requests import getQuotes
 from . import main
-from flask_login import login_required
+from flask_login import login_required,current_user
+
+
 @main.route('/')
+@main.route('/home')
 def index():
     '''
     view root page function that returns index and its data
     '''
-    return render_template('index.html')
+    quotes = getQuotes()
+    posts = Post.query.all()
+    return render_template('index.html',quotes=quotes, posts=posts, current_user=current_user)
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_path = os.path.join(app.root_path, 'static/profile_pic', picture_fn)
+
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
