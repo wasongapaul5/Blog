424b7dcddd5fb824c6969aff24684d8ae01c12c7
wasongapaul5 <wasongapaul5@gmail.com>
2019-12-02 15:47:34 +0300

    recheck on the codes to confirm the simple error mistakes

--- manage.py
+++ manage.py
@@ -1,8 +1,7 @@
-from flask_migrate import Migrate,MigrateCommand
-from flask_script import Manager,Server
-from app.models import User
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Server
 
-from app import create_app,db
+from app import create_app, db
 from app.models import User
 
 app = create_app()
@@ -10,12 +9,15 @@ app = create_app()
 manager = Manager(app)
 manager.add_command('server', Server(use_debugger=True))
 
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
 
 @manager.shell
 def add_shell_context():
-    return{'db':db,'User':User}
+    return {'db': db, 'User': User}
+
 
 if __name__ == '__main__':
     manager.run()
+ 
